e0378239ccd4b9be5662128753d7b41aa53a610c
Carlos Guerrero <git@carlosguerrero.com>
2020-04-19 19:36:30 +0100

    Include currency data in payload

--- handler.js
+++ handler.js
@@ -1,19 +1,27 @@
 'use strict';
 var geoip = require('geoip-lite');
 var countriesByAbbreviation = require("./currenciesByAbbreviation.json")
+var telegramRates = require("./telegram-rates")
 
 module.exports.geo = async event => {
+  process.env.LOG && console.log('EVENT', JSON.stringify(event, null, 2));
+
   var geoData = geoip.lookup(event.requestContext.identity.sourceIp)
+  var clientCurrency = geoData.country && countriesByAbbreviation[geoData.country]
+
+  if (clientCurrency) {
+    geoData.currency = await telegramRates(clientCurrency, event.queryStringParameters && event.queryStringParameters.baseCurrency)
+  }
+
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
     },
     body: JSON.stringify(
       {
         geo: geoData,
-        curr: geoData.country && countriesByAbbreviation[geoData.country],
         ip: event.requestContext.identity.sourceIp
       },
       null,
